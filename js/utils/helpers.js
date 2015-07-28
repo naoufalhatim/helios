ff8d970a1134c887a2d67a2696bb9b0d542e9940
Joel Hawksley <joel@mojotech.com>
2015-07-28 12:18:48 -0400

    [DESIGN] Bump up saturation

--- js/utils/helpers.js
+++ js/utils/helpers.js
@@ -2,7 +2,7 @@ function colorForTemp(tempInF) {
   var t = ((tempInF - 32) * (5 / 9));
   var hue = 30 + 240 * (31 - t) / 50;
 
-  return 'hsl(' + [hue, '90%', '50%'] + ')';
+  return 'hsl(' + [hue, '100%', '50%'] + ')';
 };
 
 export {colorForTemp};
