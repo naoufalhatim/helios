defde1cc1eab85dcba224bdab5d4e1ee202b80b1
Brian Egizi <me@brianegizi.com>
2015-10-16 21:56:41 -0400

    Use viper config options

--- api/src/helios/main.go
+++ api/src/helios/main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
-	"fmt"
 	"helios/cors"
 	"helios/github"
 	"helios/helios"
 	"helios/slack"
 	"helios/static"
 	"helios/weather"
+	"log"
 	"os"
 	"path/filepath"
 
+	"github.com/gin-gonic/gin"
 	_ "github.com/joho/godotenv/autoload"
 	"github.com/spf13/viper"
 )
@@ -20,6 +21,7 @@ func main() {
 	binPath, _ := filepath.Abs(filepath.Dir(os.Args[0]))
 
 	// Initialize Config
+	viper.SupportedExts = []string{"json"}
 	viper.SetConfigName("config")
 	viper.AddConfigPath("./")
 	viper.AddConfigPath(binPath)
@@ -27,21 +29,35 @@ func main() {
 	err := viper.ReadInConfig()
 
 	if err != nil {
-		fmt.Println(err)
+		log.Fatalln("No valid config.json file found found.")
+		os.Exit(1)
 	}
 
+	// Set Config Defaults
 	viper.SetDefault("port", "8989")
-
-	port := viper.GetString("port")
 
 	h := helios.New()
 
+	// Register http handler for sending the client config
+	h.HTTPEngine.GET("/config", func(c *gin.Context) {
+		c.JSON(200, viper.GetStringMap("client"))
+	})
+
 	h.Use("cors", cors.Service())
 	h.Use("static", static.Service())
-	h.Use("github", github.Service())
-	h.Use("slack", slack.Service())
-	h.Use("weather", weather.Service())
+
+	if viper.IsSet("forecastio.apiKey") {
+		h.Use("weather", weather.Service())
+	}
+
+	if viper.IsSet("github.apiKey") && viper.IsSet("github.apiSecret") {
+		h.Use("github", github.Service())
+	}
+
+	if viper.IsSet("slack.apiKey") {
+		h.Use("slack", slack.Service())
+	}
 
 	// Initialize helios
-	h.Run(port)
+	h.Run(viper.GetString("port"))
 }
