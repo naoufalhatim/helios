5f94e26347809f935f1ec1c24084fb68fb362766
Brian Egizi <me@brianegizi.com>
2016-07-26 14:06:33 -0400

    Allow jsx module extentensions in production build

--- client/webpack.dist.config.js
+++ client/webpack.dist.config.js
@@ -58,7 +58,7 @@ module.exports = {
 
   // Path resolving, aliases
   resolve: {
-    extensions: ["", ".js"],
+    extensions: ["", ".js", ".jsx"],
     alias: {
       components: path.resolve(__dirname, "src/components"),
       utils: path.resolve(__dirname, "src/utils"),
