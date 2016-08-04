f24e666c16174b0630036f380bbb6dfc68a5570a
Brian Egizi <me@brianegizi.com>
2016-08-04 11:54:47 -0400

    Build new layout and plugin initialization

--- client/src/components/helios/index.jsx
+++ client/src/components/helios/index.jsx
@@ -1,12 +1,9 @@
+import _ from 'lodash';
 import React from 'react';
-
-import Clock from '../clock';
-import DateStamp from '../date-stamp';
-import Activity from '../activity';
-import Weather from '../weather';
 import $ from 'jquery';
-
+import * as plugins from '../../plugins';
 import AppDispatcher from '../../dispatcher';
+import Layout from '../../layout';
 
 const CONFIG_DID_LOAD = 'CONFIG_DID_LOAD';
 
@@ -16,7 +13,20 @@ class Helios extends React.Component {
 
     this.receiveConfig = this.receiveConfig.bind(this);
 
-    this.state = {};
+    const viewPlugins = [];
+    const initializedPlugins = [];
+    _.forEach(plugins, (Plugin) => {
+      const p = new Plugin();
+      initializedPlugins.push(p);
+      if (p.view()) {
+        viewPlugins.push(p);
+      }
+    });
+
+    this.state = {
+      plugins: initializedPlugins,
+      viewPlugins
+    };
   }
 
   componentDidMount() {
@@ -25,11 +35,6 @@ class Helios extends React.Component {
         type: CONFIG_DID_LOAD,
         data: result
       });
-    });
-
-    this.commandDispatchToken = AppDispatcher.registerIfType('command', (payload) => {
-      const action = payload.action;
-      this.receiveCommand(action.data);
     });
 
     this.configDispatchToken = AppDispatcher.register((payload) => {
@@ -41,7 +46,6 @@ class Helios extends React.Component {
   }
 
   componentWillUnmount() {
-    AppDispatcher.unregister(this.commandDispatchToken);
     AppDispatcher.unregister(this.configDispatchToken);
   }
 
@@ -49,50 +53,16 @@ class Helios extends React.Component {
     this.setState(data);
   }
 
-  receiveCommand(data) {
-    // Top level command listeners
-    switch (data.name) {
-    case 'refresh':
-      // Reload the page when the "refresh" command is sent
-      location.reload();
-      break;
-    default:
-      break;
-    }
-  }
-
   render() {
     const {
       watermark,
-      firstClockOffset,
-      firstClockLabel,
-      secondClockOffset,
-      secondClockLabel
+      viewPlugins
     } = this.state;
 
     return (
       <div className="wrapper">
-        <div className="location-watermark">
-          {watermark}
-        </div>
-        <DateStamp />
-        <div className="time">
-          <div className="primary-clock">
-            <Clock width="300" height="300" />
-          </div>
-          <div className="secondary-clocks">
-            <div>
-              <Clock hourOffset={firstClockOffset} />
-              <span>{firstClockLabel}</span>
-            </div>
-            <div>
-              <Clock hourOffset={secondClockOffset} />
-              <span>{secondClockLabel}</span>
-            </div>
-          </div>
-        </div>
-        <Weather />
-        <Activity />
+        <div className="location-watermark">{watermark}</div>
+        <Layout plugins={viewPlugins} />
       </div>
     );
   }
